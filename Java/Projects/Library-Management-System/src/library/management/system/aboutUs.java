4f6ddae6f35ac50e4eab807a3519bade3093801d
Aayush Joshi <aayushj8355@gmail.com>
2020-12-21 11:10:25 +0530

    Add files via upload

---
+++ Java/Projects/Library-Management-System/src/library/management/system/aboutUs.java
@@ -0,0 +1,81 @@
+package library.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class aboutUs extends JFrame{
+
+	private JPanel contentPane;
+
+        public static void main(String[] args) {
+            new aboutUs().setVisible(true);			
+	}
+    
+        public aboutUs() {
+            
+            super("About Us");
+            setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\ram\\Desktop\\rohit.jpg"));
+            setBackground(new Color(173, 216, 230));
+            setBounds(500, 250, 700, 500);
+		
+            contentPane = new JPanel();
+            setContentPane(contentPane);
+            contentPane.setLayout(null);
+
+
+
+
+            JLabel l3 = new JLabel("Library");
+            l3.setForeground(new Color(0, 250, 154));
+            l3.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 34));
+            l3.setBounds(160, 40, 150, 55);
+            contentPane.add(l3);
+
+            JLabel l4 = new JLabel("Mangement System");
+            l4.setForeground(new Color(127, 255, 0));
+            l4.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 34));
+            l4.setBounds(70, 90, 405, 40);
+            contentPane.add(l4);
+
+            JLabel l5 = new JLabel("v1.0");
+            l5.setForeground(new Color(30, 144, 255));
+            l5.setFont(new Font("Trebuchet MS", Font.BOLD, 25));
+            l5.setBounds(185, 140, 100, 21);
+            contentPane.add(l5);
+
+
+            JLabel l6 = new JLabel("Developed By : Aayush and Group");
+            l6.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+            l6.setBounds(70, 198, 600, 35);
+            contentPane.add(l6);
+
+            JLabel l7 = new JLabel("Contact Us : aayush.joshi_19@sakec.ac.in");
+            l7.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+            l7.setBounds(70, 260, 600, 34);
+            contentPane.add(l7);
+
+            JLabel l8 = new JLabel("Contact Us : kinjan.ganatra_19@sakec.ac.in");
+            l8.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+            l8.setBounds(70, 290, 600, 34);
+            contentPane.add(l8);
+
+            JLabel l9 = new JLabel("Contact Us : rohan.dhatrak_19@sakec.ac.in");
+            l9.setFont(new Font("Trebuchet MS", Font.BOLD , 20));
+            l9.setBounds(70, 320, 600, 34);
+            contentPane.add(l9);
+
+
+            JLabel l10 = new JLabel("Library Management System Mini Project");
+            l10.setForeground(new Color(47, 79, 79));
+            l10.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 18));
+            l10.setBounds(70, 400, 600, 34);
+            contentPane.add(l10);
+                
+                
+            contentPane.setBackground(Color.WHITE);
+	}
+        
+
+}
+

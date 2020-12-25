6cda139845afa129c2d65301cba29a70d250ee52
Aayushjoshi28 <aayushj8355@gmail.com>
2020-12-25 10:10:08 +0530

    Completed the display function for queue using while loop.

--- Data Structures/1.Practice/queue_using_array_practice.c
+++ Data Structures/1.Practice/queue_using_array_practice.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define max_size 5
 int queue[max_size];
 int front = -1;
@@ -10,12 +11,26 @@ void display();
 
 void main()
 {
-
+  int ch;
+  printf("\n\t----------Queue Using Array Practice-----------")
+  while(1)
+  {
+    printf("\n\t1.Enqueue\t2.Dequeue\t3.Display\t4.Exit");
+    switch (ch)
+    {
+      case 1: enqueue(); break;
+      case 2: dequeue(); break;
+      case 3: display(); break;
+      case 4: exit(0); break;
+      default: printf("\n\tEnter Choice From the Given Options\n\t1.Enqueue\n\t2.Dequeue\n\t3.Display\n\t4.Exit"); break;
+    }
+  }
 }
 
 void enqueue()
 {
-
+  int val;
+  printf("\n\tEnter the data to enqueue:");
 }
 
 void dequeue()
@@ -25,5 +40,18 @@ void dequeue()
 
 void display()
 {
-  
+  if(front == -1 && rear == -1)
+  {
+    printf("\n\tQueue is Empty....!");
+  }
+  else
+  {
+    int i;
+    i = front;
+    while(i<rear)
+    {
+      printf("%d\t",queue[i]);
+      i++;
+    }
+  }
 }
